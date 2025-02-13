4feaae5323b14cb701f2f76f51d6897aba07d6d3
Akeen Zhong <zhongakeen@gmail.com>
2025-02-13 00:06:50 -0500

    added buttons

--- Lab1_Akeen_Zhong/ContentView.swift
+++ Lab1_Akeen_Zhong/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var score = 0
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Button("Prime: \(score)"){
+                score+=1
+            }
+            
+            Button("Not prime:  \(score)"){
+                score+=1
+                   }
         }
         .padding()
     }
