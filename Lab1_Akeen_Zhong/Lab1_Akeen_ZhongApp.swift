8a288f6bd37ee7875a369495b3a5342f4cc27b0d
Akeen Zhong <zhongakeen@gmail.com>
2025-02-12 23:28:15 -0500

    Initial Commit

---
+++ Lab1_Akeen_Zhong/Lab1_Akeen_ZhongApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Akeen_ZhongApp.swift
+//  Lab1_Akeen_Zhong
+//
+//  Created by Akeen on 2025-02-12.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Akeen_ZhongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
